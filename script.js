d7cee6c1b6e2350d50dbb5d114d9524de843f94d
21900884 <90634157+21900884@users.noreply.github.com>
2021-10-10 16:01:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function imc () {
         paragraph.textContent = `xd ${text}`;
 
     }else {
-        paragraph.textContent = 'Erro! Porfavor preencha todos os campos!';
+        resultado.write('<p>Erro! Porfavor preencha todos os campos!</p>');
     }
 
 }
