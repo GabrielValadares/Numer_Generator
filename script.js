32bdb4ea981d7d5dc5732eddfc07e8e6043b29bb
21900884 <90634157+21900884@users.noreply.github.com>
2021-10-10 15:36:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,25 @@
-const calcular = document.getElementById('calcular');
+const executar = document.getElementById('executar');
 
 
 function imc () {
-    const nome = document.getElementById('nome').value;
-    const altura = document.getElementById('altura').value;
-    const peso = document.getElementById('peso').value;
+    const numero = document.getElementById('numero').value;
     const resultado = document.getElementById('resultado');
 
-    if (nome !== '' && altura !== '' && peso !== '') {
-
-        const valorIMC = (peso/(altura**2)).toFixed(2);
+    if (numero !== '') {
         
-        const pesoMin = (18.50 * (altura**2)).toFixed(2);
+        var i = 0;
         
-        const pesoMax = (24.99 * (altura**2)).toFixed(2);
-        
-        let classificacao = '';
-
-        if (valorIMC < 18.5){
-            classificacao = 'abaixo do peso.';
-        }else if (valorIMC < 25) {
-            classificacao = 'com o peso ideal. Parabéns!';
-        }else if (valorIMC < 30){
-            classificacao = 'levemente acima do peso.';
-        }else if (valorIMC < 35){
-            classificacao = 'com obesidade grau I.';
-        }else if (valorIMC < 40){
-            classificacao = 'com obesidade grau II.';
-        }else {
-            classificacao = 'com obesidade grau III.';
+        while (i < numero) {
+            
+            text += i + ") <br><br><br>";
+            i++;
         }
-
-        resultado.textContent = `Olá, ${nome}! Com ${peso}kg e ${altura}m, seu IMC é ${valorIMC}. Atualmente, você está ${classificacao} Procure manter o seu peso entre ${pesoMin} kg e ${pesoMax} kg.`;
+        
+        resultado.textContent = `${text}`;
 
     }else {
-        resultado.textContent = 'Para calcular o seu IMC , preencha todos os campos.';
+        resultado.textContent = 'Erro! Porfavor preencha todos os campos!';
     }
 
 }
-calcular.addEventListener('click', imc);
+executar.addEventListener('click', imc);
